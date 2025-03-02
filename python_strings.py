449e3f062683daa976333a2719f825774d35caba
holden-it-down <kenzmaki9@gmail.com>
2025-03-02 19:41:25 +0900

    Create python_strings.py

---
+++ python_strings.py
@@ -0,0 +1,9 @@
+
+# Python strings and its methods
+
+
+# Q1 how to replace certain char or chars of a string?
+
+string = string.replace("What you want replaced", "What you want it to be replaced to")
+
+# Q2
