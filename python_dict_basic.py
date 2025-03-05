7ad5c756f4a71ad89e11d717170597024f60ed5f
holden-it-down <kenzmaki9@gmail.com>
2025-03-05 16:29:55 +0900

    Update python_dict_basic.py

--- python_dict_basic.py
+++ python_dict_basic.py
@@ -25,3 +25,17 @@ distances.update("key", "value")
 # get a singlue value off the key u know
 
 distances.get("keyvalue") # returns the value if exists.
+
+# going through all keys and values with dict.items()
+
+for a, b in dict.items():
+    print(a, b)
+
+# Removing a certain key element pair on your dict with DICT.POP()
+
+distances.pop("keyname") 
+
+
+# How to delete the dict or the contents of all the keys and values
+
+distances.clear()
