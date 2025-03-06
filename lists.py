4c37af58b2443eca04127cae7c90e7777a6bc8ee
holden-it-down <kenzmaki9@gmail.com>
2025-03-06 19:53:27 +0900

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,19 @@
+
+
+results = ["Jack", "Lassig"]
+
+# Append to the end of a list
+results.append("Kazu")
+
+# removes an item from a list, only removes the first Kazu from the list
+results.remove("Kazu")
+
+
+# adds individual items of a list to a list
+results.extend(["Mark", "Jack"])
+
+# Inserting an element to the index you specify
+results.insert(0, "John")
+
+# Flips the order of a result
+result.reverse()
