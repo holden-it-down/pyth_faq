5c44679cc58c8ee4383b1bdb03443b19a9a42f73
holden-it-down <kenzmaki9@gmail.com>
2025-03-07 23:03:38 +0900

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,15 @@
+--- Working with Python Strings and its methods
+
+1q. How do I capitalize a string? 1a. string.capitalize()
+
+2q. How do I remove whitespaces from a string? 2a. string.strip()
+
+3q. How do I change the behavior of the print function's ending? 3a. The print function takes 3 arguments, the first to print, sep="" set to default, and an end="\n" set to default
+
+print("cool", end="123") will give you "cool123"
+
+4q. How would I replace a part of a string in python? 4a. string = string.replace("what you want to replace", "what you want it to be replaced with")
+
+5q. How would I remove a prefix of a string? 5a. new_string = string.removeprefix('whatyouwantrmeoved')
+
+6q. How would i remove a suffix of a string 6a. new_string = string.removesuffix('whatyouwantremoved')
