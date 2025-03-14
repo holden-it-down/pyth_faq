928546798ce1399cda3e254fdac8eec0ccbffeaa
holden-it-down <kenzmaki9@gmail.com>
2025-03-14 23:37:40 +0900

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,28 +1,9 @@
-# function def
+# Function definition
 
 def function_name():
-  print(1)
+  print("This is how you define a function")
 
 
-x = 5
-print(x)
-# function scopes
-def func_main():
-  x = 3
-  print(x)
+# Function Call
+function_name()
 
-
-func_main() # 3
-
-
-# Work on multi calls
-
-def main()
-  print(1)
-  print(2)
-
-def call_main()
-  print(3)
-  main()
-
-call_main()
